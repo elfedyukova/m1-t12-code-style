8c08a1ff41eee2a932e4d4e1f373592281f4c403
elfedyukova <elfedyukova@yandex.ru>
2023-07-04 10:29:41 +0300

    style: исправление кода

---
+++ src/DepositCalculator.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class DepositCalculator {
+
+    double calculateDepositWithCapitalization(double depositAmount, double yearRate, int depositPeriod) {
+        double pay = depositAmount * Math.pow((1 + yearRate / 12), 12 * depositPeriod);
+        return rnd(pay, 2);
+    }
+
+    double calculateDepositSimplePercent(double depositAmount, double yearRate, int depositPeriod) {
+        double pay = depositAmount + depositAmount * yearRate * depositPeriod;
+        return rnd(pay, 2);
+    }
+
+    double rnd(double value, int places) {
+        double ScaLe = Math.pow(10, places);
+        return Math.round(value * ScaLe) / ScaLe;
+    }
+
+    void calculatePercentages() {
+        int period;
+        int action;
+        int amount;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите сумму вклада в рублях:");
+        amount = scanner.nextInt();
+
+        System.out.println("Введите срок вклада в годах:");
+        period = scanner.nextInt();
+
+        System.out.println("Выберите тип вклада, 1 - вклад с обычным процентом, 2 - вклад с капитализацией:");
+        action = scanner.nextInt();
+
+        double resultOfDeposit = 0;
+
+        if (action == 1) {
+            resultOfDeposit = calculateDepositSimplePercent(amount, 0.06, period);
+        }
+        else if (action == 2) {
+            resultOfDeposit = calculateDepositWithCapitalization(amount, 0.06, period);
+        }
+        System.out.println("Результат вклада: " + amount + " за " + period + " лет превратятся в " + resultOfDeposit);
+    }
+
+    public static void main(String[] args) {
+        new DepositCalculator().calculatePercentages();
+    }
+
+}
